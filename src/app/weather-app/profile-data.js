6b49b10d7b9274f8aafa2c8fd57b22bf0c0bd37a
Vigneshwaran B <vignesh-2330@vignesh-2330.csez.zohocorpin.com>
2016-09-19 20:01:52 +0530

    Remove unwanted files

--- src/app/weather-app/profile-data.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-var weather_profiles_1 = require("./weather-profiles/weather-profiles");
-exports.PROFILE_DATA = [
-    new weather_profiles_1.WeatherProfiles("Default Profile", ['Chennai', 'Madurai', 'Coimbatore'])
-];
-//# sourceMappingURL=profile-data.js.map
--- src/app/weather-app/weather-data.js
+++
@@ -1,3 +0,0 @@
-"use strict";
-exports.WEATHER_DATA = [];
-//# sourceMappingURL=weather-data.js.map
